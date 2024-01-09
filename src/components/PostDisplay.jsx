02e88f46cd2afaa8d21645f0dd71b58f2f78ff65
Daniel Karlsson <daniel.karlsson36@outlook.com>
2024-01-09 18:55:29 +0100

    Multiple posts.

--- src/components/PostDisplay.jsx
+++ src/components/PostDisplay.jsx
@@ -1,55 +1,166 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style/PostDisplay.css";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { arta } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-const codeString = `
-  function BFS(start){
-    let queue = [start];
-    let visited = {};
-    
-    while(queue.length > 0){
-      let current = queue.shift();
-    
-      if(current in visited) continue;
-      visited[current] = true;  
+export default function PostDisplay() {
+  let [posts, setPosts] = useState({
+    Graph: {
+      posts: [
+        {
+          label: "Dijkstras",
+          codeBlocks: [
+            {
+              description:
+                "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+              code: `function BFS(start){
+  let queue = [start];
+  let visited = {};
+  
+  while(queue.length > 0){
+    let current = queue.shift();
+  
+    if(current in visited) continue;
+    visited[current] = true;  
 
-      for(let neighbor in current.neighbors){
-        queue.push(neighbour);
-      }
+    for(let neighbor in current.neighbors){
+      queue.push(neighbour);
     }
   }
-`;
+}`,
+            },
+            {
+              description:
+                "This is a modified version of the Dijkstras algorithm.",
+              code: "let queue = true;",
+            },
+          ],
+        },
+        {
+          label: "A*",
+          codeBlocks: [
+            {
+              description:
+                "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+              code: `function BFS(start){
+  let queue = [start];
+  let visited = {};
+  
+  while(queue.length > 0){
+    let current = queue.shift();
+  
+    if(current in visited) continue;
+    visited[current] = true;  
 
-export default function PostDisplay() {
-  return (
-    <div className="postDisplayContainer">
-      <div className="categoryContainer">
-        <div className="categoryHeaderContainer">
-          <div className="categoryHeader">Graph</div>
-        </div>
-        <div className="postContainer">
-          <div className="postHeaderContainer">
-            <p className="postHeader">Dijkstras Algorithm</p>
-          </div>
+    for(let neighbor in current.neighbors){
+      queue.push(neighbour);
+    }
+  }
+}`,
+            },
+            {
+              description:
+                "This is a modified version of the Dijkstras algorithm.",
+              code: "let queue = true;",
+            },
+          ],
+        },
+      ],
+    },
+    Math: {
+      posts: [
+        {
+          label: "Dijkstras",
+          codeBlocks: [
+            {
+              description:
+                "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+              code: `function BFS(start){
+  let queue = [start];
+  let visited = {};
+  
+  while(queue.length > 0){
+    let current = queue.shift();
+  
+    if(current in visited) continue;
+    visited[current] = true;  
 
-          <div className="descAndCodeWrapper">
-            <div className="postDescriptionContainer">
-              <p className="postDescription">
-                Lorem ipsum dolor sit amet, qui minim labore adipisicing minim
-                sint Lorem ipsum dolor sit amet, qui minim labore adipisicing
-                minim sint cillum sint consectetur cupidatat.
-              </p>
+    for(let neighbor in current.neighbors){
+      queue.push(neighbour);
+    }
+  }
+}`,
+            },
+            {
+              description:
+                "This is a modified version of the Dijkstras algorithm.",
+              code: "let queue = true;",
+            },
+          ],
+        },
+      ],
+    },
+  });
+
+  useEffect(() => {
+    setPosts((prev) => prev);
+  }, []);
+
+  function renderPosts() {
+    let categoryJsx = [];
+
+    for (let category in posts) {
+      let postJsx = [];
+
+      for (let post of posts[category].posts) {
+        let codeBlocksJsx = [];
+
+        for (let codeBlock of post.codeBlocks) {
+          let tmpCodeBlockJsx = (
+            <div className="descAndCodeWrapper">
+              <div className="postDescriptionContainer">
+                <p className="postDescription">{codeBlock.description}</p>
+              </div>
+
+              <div className="postCodeContainer">
+                <SyntaxHighlighter language="javascript" style={arta}>
+                  {codeBlock.code}
+                </SyntaxHighlighter>
+              </div>
+            </div>
+          );
+
+          codeBlocksJsx.push(tmpCodeBlockJsx);
+        }
+
+        let newPost = (
+          <div className="postContainer">
+            <div className="postHeaderContainer">
+              <p className="postHeader">{post.label}</p>
             </div>
 
-            <div className="postCodeContainer">
-              <SyntaxHighlighter language="javascript" style={arta}>
-                {codeString}
-              </SyntaxHighlighter>
-            </div>
+            {codeBlocksJsx}
           </div>
+        );
+
+        postJsx.push(newPost);
+      }
+
+      let tmpCategoryJsx = (
+        <div className="categoryContainer">
+          <div className="categoryHeaderContainer">
+            <div className="categoryHeader">{category}</div>
+          </div>
+
+          {postJsx}
         </div>
-      </div>
-    </div>
-  );
+      );
+
+      categoryJsx.push(tmpCategoryJsx);
+    }
+
+    return categoryJsx;
+  }
+
+  return <div className="postDisplayContainer">{renderPosts()}</div>;
 }
